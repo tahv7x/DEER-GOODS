c73cc8243b2e1b771aa52dfd91b487744871291d
tahv7x <thassbii@gmail.com>
2026-06-14 16:22:34 +0100

    adding modification to index.ts route

--- backend/index.ts
+++ backend/index.ts
@@ -15,7 +15,7 @@ const app = express();
 app.use(helmet());
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL ? process.env.FRONTEND_URL.replace(/\/$/, '') : 'http://localhost:3000',
   credentials: true
 }));
 app.use(express.json());
